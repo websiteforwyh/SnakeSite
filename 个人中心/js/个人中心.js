aa3a2e0acffa640b41fd8cb7d5007919aac23586
=wyh <=2085195880@qq.com>
2024-05-19 22:17:12 +0800

    1

---
+++ 个人中心/js/个人中心.js
@@ -0,0 +1,22 @@
+﻿
+function addSnack() {
+    var snackName = prompt('请输入要添加的零食名称：');
+    if (snackName) {
+        var snackList = document.getElementById('snackList');
+        var newSnack = document.createElement('li');
+        newSnack.textContent = snackName;
+        snackList.appendChild(newSnack);
+    }
+}
+
+function removeSnack() {
+    var snackList = document.getElementById('snackList');
+    var lastSnack = snackList.lastElementChild;
+    if (lastSnack) {
+        snackList.removeChild(lastSnack);
+    } else {
+        alert('没有可删除的零食！');
+    }
+}
+
+alert("欢迎来到零食网页个人中心！");

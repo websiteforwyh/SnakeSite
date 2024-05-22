ad2ef62c8fc54fa1effcb7fd0e52d6edf305e8af
wyh <wyh@qq.com>
2024-05-22 10:49:33 +0800

    1

--- 注册登录表单/js/前后端数据交互.js
+++ 注册登录表单/js/前后端数据交互.js
@@ -17,16 +17,16 @@ function RegisterRequest() {
     xhr.open('POST', 'http://127.0.0.1:5000/register', true);
     xhr.setRequestHeader('Content-Type', 'application/json');
 
-    xhr.onreadystatechange = function () {
+    xhr.onreadystatechange = function() {
         if (xhr.readyState === 4 && xhr.status === 200) {
             // 处理服务器返回的数据
             var response = JSON.parse(xhr.responseText);
-            if (response.success) {     // 注册成功
+            if (response.success) { // 注册成功
                 alert(response.message);
                 //  注册成功后保存登录状态到LocalStorage
                 sessionStorage.setItem('isLoggedIn', 'true');
-                window.location.href = '../好恰恰零食首页.html';
-            } else {    // 注册失败
+                window.location.href = '../index.html';
+            } else { // 注册失败
                 alert(response.message);
             }
         }
@@ -51,15 +51,15 @@ function LoginRequest() {
     xhr.open('POST', 'http://127.0.0.1:5000/login', true);
     xhr.setRequestHeader('Content-Type', 'application/json');
 
-    xhr.onreadystatechange = function () {
+    xhr.onreadystatechange = function() {
         if (xhr.readyState === 4 && xhr.status === 200) {
             // 处理服务器返回的数据
             var response = JSON.parse(xhr.responseText);
             if (response.success) { // 登录成功
                 alert(response.message);
-                sessionStorage.setItem('isLoggedIn', 'true');   // 登录成功后保存登录状态到LocalStorage
-                window.location.href = '../好恰恰零食首页.html';    // 加载新页面
-            } else {    // 登录失败
+                sessionStorage.setItem('isLoggedIn', 'true'); // 登录成功后保存登录状态到LocalStorage
+                window.location.href = '../index.html'; // 加载新页面
+            } else { // 登录失败
                 alert(response.message);
             }
         }
---
+++ 首页附件/js/屏幕兼容.js
@@ -0,0 +1,22 @@
+var s = window.screen; // 获取屏幕的属性
+
+// 私人平板电脑屏幕大小（width=1344，height=840，360浏览器）
+if (s.height <= 840 && s.width <= 1344) {
+    $('.featured-dish h2').css('margin', '28px 0px 0 30px');
+    $('.one').css('top', '445px');
+    $('.one').css('left', '1%');
+    $(".content").css('height', '400px');
+} else {
+    $('.featured-dish h2').css('margin', '51px 0px 0 901px');
+    $('.one').css('top', '570px');
+    $('.one').css('left', '18%');
+
+    $('#show').css('height', '400px');
+    $('#slider img').css('height', '400px');
+    $('#slider').css('height', '400px');
+    $(".content").css('height', '400px');
+    $(".content").css('position', 'relative');
+    $(".content").css('top', '20px');
+
+
+}

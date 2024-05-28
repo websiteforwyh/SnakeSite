bbe2eaa4bd76b7e3b5e6d670da241951f8eda48e
gitwyh <2085195880@qq.com>
2024-05-28 23:29:03 +0800

    1

--- Sign/js/前后端数据交互.js
+++ Sign/js/前后端数据交互.js
@@ -4,34 +4,26 @@ function RegisterRequest() {
     var password = document.getElementById("register_password").value;
     var email = document.getElementById("email").value;
 
-    // 构建发送到服务器的数据
-    var data = {
-        username: username,
-        password: password,
-        email: email
-    };
-
     // 通过 Ajax 发送数据到服务器
     var xhr = new XMLHttpRequest();
     xhr.open('POST', 'http://127.0.0.1:5000/register', true);
-    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
 
     xhr.onreadystatechange = function () {
-        if (xhr.readyState === 4 && xhr.status === 200) {
+        if (xhr.status === 200) {
             // 处理服务器返回的数据
-            var response = JSON.parse(xhr.responseText);
-            if (response.success) { // 注册成功
-                alert(response.message);
-                SetLocalStorageData(); // 储存信息到localStorage
-                sessionStorage.setItem('isLoggedIn', 'true'); // 临时保存登录状态
-                sessionStorage.setItem('login_username', username); // 临时保存用户名
-                window.location.href = '../index.html';
-            } else { // 注册失败
-                alert(response.message);
-            }
+            var response = xhr.responseText;
+            alert(response);
+            SetLocalStorageData(); // 储存信息到localStorage
+            sessionStorage.setItem('isLoggedIn', 'true'); // 临时保存登录状态
+            sessionStorage.setItem('login_username', username); // 临时保存用户名
+            window.location.href = '../index.html';
+        } else { // 注册失败
+            alert("失败");
         }
-    };
-    xhr.send(JSON.stringify(data));
+    }
+    var str = 'username=' + username + '&password=' + password + '&email=' + email;
+    xhr.send(str);
 }
 
 // 登录
